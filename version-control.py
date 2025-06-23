58e4b71a00f468699d1f06bf240151e572918737
bndr-966 <alyamibandr@gmail.com>
2025-06-23 15:50:24 +0300

    Changes to be committed: 	modified:   version-control.py      I make class for culcolate age and funcation

--- version-control.py
+++ version-control.py
@@ -1,2 +1,19 @@
-print("first message")
-print("second message")
+from datetime import datetime
+
+class Age:
+    def __init__(self, name: str, birthdate: str):
+        self.name = name
+        # تحويل النص إلى تاريخ
+        self.birthdate = datetime.strptime(birthdate, "%Y-%m-%d")
+        
+    def calculate_age(self):
+        today = datetime.today()
+        age = today.year - self.birthdate.year
+        # التأكد من أن يوم الميلاد قد حدث هذا العام أو لا
+        if (today.month, today.day) < (self.birthdate.month, self.birthdate.day):
+            age -= 1
+        return age
+
+# مثال على الاستخدام:
+person = Age("Bandr", "2001-2-4")
+print(f"{person.name} is {person.calculate_age()} years old.")
